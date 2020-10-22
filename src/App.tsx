a487c49d5757db0dcd77dfbaf31a5aee20bed114
schreyerpeter <pschreyer@weedmaps.com>
2020-10-22 11:52:07 -0700

    Swap out icons and restructure landing page

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app-header">
+        <div>
+          <img src={logo} className="app-logo" alt="logo" />
+        </div>
+        <div>
+          InsureStellar{' '}
+          <span role="img" aria-label="🚀">
+            🚀
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
