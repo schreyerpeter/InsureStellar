6c549261380a78ad5d11b76a6b9d7a25af64a752
schreyerpeter <pschreyer@weedmaps.com>
2020-10-25 10:53:27 -0700

    Node server setup

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', function (req, res, next) {
+  res.sendStatus(200);
+});
 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
