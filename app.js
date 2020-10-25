922eb09d84e23977fffd76142b980643880f5acb
schreyerpeter <pschreyer@weedmaps.com>
2020-10-25 11:47:49 -0700

    WIP

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-var publicPath = path.join(__dirname, '..', 'public');
+var publicPath = path.join(__dirname, 'public');
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
