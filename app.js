57a15b384c707ab3f598ea1b3cb6dcfcbb8c9d25
schreyerpeter <pschreyer@weedmaps.com>
2020-10-25 10:15:38 -0700

    Add express

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type',
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+var PORT = process.env.PORT || 8080;
+app.listen(PORT);
